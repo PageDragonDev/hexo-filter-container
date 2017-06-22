3f142af49134a68943a2e75cf66e1a52a705380b
Jerry Holmes <jholmes@pagedragon.com>
2017-06-22 22:20:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/* global hexo */
+
+var assign = require('object-assign');
+var markup = ':::';
+
+hexo.config.sup = assign({ markup: markup }, hexo.config.sup);
+var placeholder = (hexo.config.sup.markup || markup)
+  .split('')
+  .map(function (char) {
+    return '\\' + char;
+  })
+  .join('');
+
+var reg = new RegExp(placeholder + '(.*?)' + placeholder, 'g');
+
+function parse(data) {
+  var source = data.source;
+  var ext = source.substring(source.lastIndexOf('.')).toLowerCase();
+
+  if (['.js', '.css', '.html', '.htm'].indexOf(ext) > -1) {
+    return;
+  }
+
+  // 29^th^ => 29<sup>th</sup>
+  data.content = data.content.replace(reg, function (raw, content) {
+    return '<div class="block">' + content + '</div>';
+  });
+}
+
+hexo.extend.filter.register('before_post_render', parse, 9);
