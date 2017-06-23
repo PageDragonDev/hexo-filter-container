bb65f01de94738e1dc9d98b583d36dd19b5b5e12
Jerry Holmes <jholmes@pagedragon.com>
2017-06-23 03:51:11 +0000

    Base version

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 /* global hexo */
-console.log("I'M HERE")
 
 var assign = require('object-assign');
-var markup = ':::';
+var markup = '^\:\:\:(.*)';
 
-hexo.config.sup = assign({ markup: markup }, hexo.config.sup);
-var placeholder = (hexo.config.sup.markup || markup)
-  .split('')
-  .map(function (char) {
-    return '\\' + char;
-  })
-  .join('');
-
-var reg = new RegExp(placeholder + '(.*?)' + placeholder, 'g');
+hexo.config.container = assign({ markup: markup }, hexo.config.container);
+var placeholder = (hexo.config.container.markup || markup);
+var reg = new RegExp(placeholder, 'gm');
 
 function parse(data) {
   var source = data.source;
   var ext = source.substring(source.lastIndexOf('.')).toLowerCase();
-
+  
   if (['.js', '.css', '.html', '.htm'].indexOf(ext) > -1) {
     return;
   }
 
-  // 29^th^ => 29<sup>th</sup>
-  data.content = data.content.replace(reg, function (raw, content) {
-    return '<div class="block">' + content + '</div>';
+  var changed = false;
+  var returnedMarkup = data.content.replace(reg, function (raw, classNames) {
+    classNames = classNames.trim();
+
+    var ret = "";
+    if(classNames.length > 0) {
+      ret  = '{% raw %}<div class="' + classNames + '">{% endraw %}';
+    } else {
+      ret =  '{% raw %}</div>{% endraw %}';
+    }
+    changed = true;
+    return ret;
   });
+  
+  if(changed) {
+    data.content = returnedMarkup;
+  }
 }
 
 hexo.extend.filter.register('before_post_render', parse, 9);
